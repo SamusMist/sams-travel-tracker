11d54eee711d0d67c78d979b94752e4e01c5c042
Sam Mistele <smmstl0@gmail.com>
2022-03-02 16:08:59 -0700

    Add sample data

--- src/Traveler.js
+++ src/Traveler.js
@@ -1,8 +1,5 @@
 class Traveler {
-  constructor(travelerApi) {
-    this.id = travelerApi.id,
-    this.name = travelerApi.name,
-    this.travelerType = travelerApi.travelerType
+  constructor() {
   }
 }
 
--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -1,1 +1,18 @@
 import { expect } from 'chai';
+import traveler from '../src/sample-data/Traveler-sample.js';
+import Traveler from '../src/Traveler.js';
+
+describe('Traveler', () => {
+  let traveler1, traveler2, traveler3
+
+    beforeEach(() => {
+      traveler1 = new Traveler(traveler[0]);
+      traveler2 = new Traveler(traveler[1]);
+      traveler3 = new Traveler(traveler[2]);
+    });
+
+  it('should be a function', () => {
+    expect(Traveler).to.be.a('function');
+  })
+
+})
