10a6619074e19c883813233832b985c9e68a53b0
Sam Mistele <smmstl0@gmail.com>
2022-03-03 16:52:11 -0700

    Add data fetch and promise

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,17 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
-
+import './images/turing-logo.png';
+import Destinations from './Destinations.js';
+import Traveler from './Traveler.js';
+import Trips from './Trips.js';
+import {fetchData} from './apiCalls.js';
+import './images/turing-logo.png';
+import {} from './domUpdates.js'
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
 
-
-console.log('This is the JavaScript entry file - your code begins here.');
+let makePromise = () => {
+  Promise.all([fetchData('travelers'), fetchData('trips'), fetchData('destinations')]).then(data => {
+  let newTraveler = new Traveler(data[0].travelers);
+  let newTrip = new Trips(data[1].trips);
+  let newDestination = new Destination(data[2].destinations);
+  })
+};
