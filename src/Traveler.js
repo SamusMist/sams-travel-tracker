2c11fb3ca97f124298fb9e64670c08567b7a386e
Sam Mistele <smmstl0@gmail.com>
2022-03-04 16:20:20 -0700

    Add annual cost method

--- src/Traveler.js
+++ src/Traveler.js
@@ -3,7 +3,50 @@ class Traveler {
     this.id = traveler.id;
     this.name = traveler.name;
     this.travelerType = traveler.travelerType;
+    this.trips = [];
   }
+
+  addDestinationToTrip(allDest) {
+    const getTrip = this.trips.filter(currTrip => this.id === currTrip.userID);
+    getTrip.forEach(trip => {
+      allDest.forEach(dest => {
+        if(dest.id === trip.destinationID) {
+          trip.destination = dest
+          }
+        })
+      })
+    }
+
+
+  getPendingTripsByUserID(allTrips) {
+    const getTrip = allTrips.filter(currTrip => this.id === currTrip.userID);
+    getTrip.forEach(trip => {
+      if(trip.status === 'pending'){
+        this.trips.push(trip)
+      }
+    })
+    return this.trips
+  }
+
+  matchDestInfoToTrip() {
+    let destInfo = []
+    this.getPendingTripsByUserID().forEach(currPendTrip => {
+      this.destinations.forEach(currDest => {
+        if(currDest.id === currPendTrip.destinationID) {
+          destInfo.push(currDest)
+        }
+      })
+    })
+    return destInfo
+  }
+
+  annualCost() {
+    const result = this.trips.forEach(currTrip => {
+        if(currTrip.destination.id === currTrip.destinationID) {
+          currTrip.totalCost = currTrip.destination.estimatedLodgingCostPerDay + currTrip.destination.estimatedFlightCostPerPerson
+        }
+      })
+    }
 }
 
 export default Traveler;
--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,26 +1,37 @@
+import Traveler from './Traveler.js';
+
 let mainData = document.querySelector('.main-data');
 let pastTrips = document.querySelector('.past-trips');
 let presentTrips = document.querySelector('.present-trips');
 let futureTrips = document.querySelector('.future-trips');
+let pending = document.querySelector('.pending');
 
 
-// const pendingTripDataDom = () => {
-//   mainData.innerHTML += `
-//   <div class="pending">
-//   <p>pending trips:</p>
-//   <p>${method that shows all pending trips for user}</p>
-//   </div>
-//   `
-// }
-//
-// const annualCostDataDom = () => {
-//   mainData.innerHTML += `
-//   <div class="annual-cost">
-//   <p>annual cost:</p>
-//   <p>${method that shows total spent on trips for user}</p>
-//   </div>
-//   `
-// }
+const pendingTripDataDom = (person, tripData, destData) => {
+  person.getPendingTripsByUserID(tripData).forEach(trip => {
+    person.addDestinationToTrip(destData)
+    console.log(trip)
+    pending.innerHTML += `
+    <div class="pending-data">
+      <p>${trip.date}</p>
+      <p>${trip.duration} days</p>
+      <p>${trip.travelers} travelers</p>
+      <p>${trip.destination.destination}</p>
+      <img src="${trip.destination.image} alt=${trip.destination.alt}">
+    </div>
+    `
+
+  })
+}
+
+const annualCostDataDom = () => {
+  mainData.innerHTML += `
+  <div class="annual-cost">
+  <p>annual cost:</p>
+  <p>${method that shows total spent on trips for user}</p>
+  </div>
+  `
+}
 
 // const pastTripsDom = () => {
 //   pastTrips.innerHTML += `
@@ -39,4 +50,4 @@ let futureTrips = document.querySelector('.future-trips');
 //   `
 // }
 
-export {pendingTripData, annualCostData}
+export {pendingTripDataDom}
