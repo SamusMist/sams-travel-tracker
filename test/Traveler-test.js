4bbac1874a782b4f821dd558d8de099b1d849aa4
Sam Mistele <smmstl0@gmail.com>
2022-03-05 19:21:29 -0700

    Add test for get future trips method

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -199,4 +199,62 @@ describe('Traveler', () => {
     }
   }])
   })
+
+  it('should return all future trips for a user', () => {
+    traveler1.addUserTrips(trips);
+    expect(traveler1.getFutureTrips()).to.deep.equal([{
+    id: 1,
+    userID: 1,
+    destinationID: 49,
+    travelers: 1,
+    date: '2022/09/16',
+    duration: 8,
+    status: 'approved',
+    suggestedActivities: [],
+    destination: {
+      id: 49,
+      destination: 'Lima, Peru',
+      estimatedLodgingCostPerDay: 70,
+      estimatedFlightCostPerPerson: 400,
+      image: 'https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80',
+      alt: 'overview of city buildings with a clear sky'
+    }
+  },
+  {
+    id: 2,
+    userID: 1,
+    destinationID: 25,
+    travelers: 5,
+    date: '2022/10/04',
+    duration: 18,
+    status: 'approved',
+    suggestedActivities: [],
+    destination: {
+      id: 25,
+      destination: 'Stockholm, Sweden',
+      estimatedLodgingCostPerDay: 100,
+      estimatedFlightCostPerPerson: 780,
+      image: 'https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+      alt: 'city with boats on the water during the day time'
+    }
+  },
+  {
+    id: 3,
+    userID: 1,
+    destinationID: 22,
+    travelers: 4,
+    date: '2022/05/22',
+    duration: 17,
+    status: 'pending',
+    suggestedActivities: [],
+    destination: {
+      id: 22,
+      destination: 'Sydney, Austrailia',
+      estimatedLodgingCostPerDay: 130,
+      estimatedFlightCostPerPerson: 950,
+      image: 'https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+      alt: 'opera house and city buildings on the water with boats'
+    }
+  }])
+  })
 })
